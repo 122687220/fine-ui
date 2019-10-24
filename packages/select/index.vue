9e03fddb8264bbb460af557e280edb6c7e6a72e2
景 <>
2019-10-24 10:58:46 +0800

    release: 0.1.1

--- packages/select/index.vue
+++ packages/select/index.vue
@@ -3,10 +3,10 @@
     <div class="zv-select-default">
       <el-select v-model="currentValue" v-bind="$attrs" v-on="$listeners">
         <el-option
-          v-for="item in options"
+          v-for="(item, index) in options"
           :key="item.value ? item.value : index"
           :label="item.label ? item.label : item"
-          :value="item.value ? item.value : item"
+          :value="item.value ? item.value : index"
         >
         </el-option>
       </el-select>
